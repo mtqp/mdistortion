ba30841042485bc0604e185578ce7565f486630a
marian.sabianAA <marian.sabianAA@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-04-04 17:46:57 +0000

    ajap

--- trunk/interface_m_dist/main.c
+++ trunk/interface_m_dist/main.c
@@ -16,17 +16,86 @@
 
 
 #include "m_distortion.h"
-//#include "callbacks.c"
+//#include "callbacks.h"
 
 
 #include <jack/jack.h>
 #include <gtk/gtk.h>
+
+#define sensitivo true;
+#define no_sensitivo false;
 
 jack_port_t *input_port;
 jack_port_t *output_left;
 jack_port_t *output_right;
 
 m_distortion *m_dist; 
+
+////////////////////////////////////////////
+///////////CALLBACKS-INTERFAZ///////////////
+////////////////////////////////////////////
+
+//cierra con la X
+G_MODULE_EXPORT bool on_m_distortion_destroy (GtkObject *object, gpointer user_data)
+{
+    gtk_main_quit();
+	return false;	
+}
+
+//cierra con el boton QUIT
+G_MODULE_EXPORT bool  on_quit_clicked( GtkButton *button, gpointer   data ) {
+    gtk_main_quit ();
+    return false;
+}
+
+//////////MODOS//////////////////
+G_MODULE_EXPORT void on_rock_mode_clicked (gpointer distors, GtkRadioButton *button){
+	g_print("CLICKED ROCK AND ROOL BABY\n");
+	gtk_widget_set_sensitive((GtkWidget*) distors, true);//sensitivo);
+	set_m_distortion(m_dist, -1);
+}
+
+G_MODULE_EXPORT void on_random_mode_clicked ( gpointer distors,GtkRadioButton *button){
+	g_print("CLICKED random mode\n");
+	gtk_widget_set_sensitive((GtkWidget*) distors, false);//no_sensitivo);
+	set_m_distortion(m_dist,7);//e_random_day);		//quizas es mejor hacer un enum de los numeros para q se entienda mas!
+}
+
+G_MODULE_EXPORT void on_mute_mode_clicked ( gpointer distors,GtkRadioButton *button){
+	g_print("CLICKED muteeeee\n");
+	gtk_widget_set_sensitive((GtkWidget*) distors, false);//no_sensitivo);
+	set_m_distortion(m_dist, 8);//e_mute);
+}
+
+G_MODULE_EXPORT void on_by_pass_mode_clicked ( gpointer distors, GtkRadioButton *button){
+	g_print("CLICKED by_pass\n");
+	gtk_widget_set_sensitive((GtkWidget*) distors, false);//no_sensitivo);
+	set_m_distortion(m_dist, 9);//e_by_pass);
+}
+
+
+//////////////DISTORSIONES/////////////////
+G_MODULE_EXPORT void on_log_rock_clicked(gpointer distors,GtkRadioButton *button){
+	set_m_distortion(m_dist, 0);//e_log_rock);
+}
+G_MODULE_EXPORT void on_log_rock_II_clicked(gpointer distors,GtkRadioButton *button){
+	set_m_distortion(m_dist, 1);//e_log_rock_II);
+}
+G_MODULE_EXPORT void on_hell_sqrt_clicked(gpointer distors,GtkRadioButton *button){
+	set_m_distortion(m_dist, 2);//e_hell_sqrt);
+}
+G_MODULE_EXPORT void on_psychedelic_if_clicked(gpointer distors,GtkRadioButton *button){
+	set_m_distortion(m_dist, 3);//e_psychedelic_if);
+}
+G_MODULE_EXPORT void on_by_60s_clicked(gpointer distors,GtkRadioButton *button){
+	set_m_distortion(m_dist, 4);//e_by_60s);
+}
+G_MODULE_EXPORT void on_fuzzy_dark_pow_IV_clicked(gpointer distors,GtkRadioButton *button){
+	set_m_distortion(m_dist, 5);//e_fuzzy_dark_pow_IV);
+}
+G_MODULE_EXPORT void on_rare_cuadratic_clicked(gpointer distors,GtkRadioButton *button){
+	set_m_distortion(m_dist, 6);//e_rare_cuadratic);
+}
 
 ////////////////////////////////////////////
 ///////////CALLBACKS-JACK///////////////////
@@ -55,29 +124,6 @@ void jack_shutdown (void *arg) {
 	exit (1);
 }
 
-////////////////////////////////////////////
-///////////CALLBACKS-INTERFAZ///////////////
-////////////////////////////////////////////
-
-bool on_m_distortion_destroy (GtkObject *object, gpointer user_data)
-{
-    gtk_main_quit();
-	return false;	
-}
-
-
-
-//* Our callback.
-// * The data passed to this function is printed to stdout 
-static void callback( GtkWidget *widget, gpointer   data ) {
-    g_print ("Hello again - %s was pressed\n", (char *) data);
-}
-
-//* This callback quits the program 
-bool on_quit_clicked( GtkButton *button/*, gpointer   data*/ ) {
-    gtk_main_quit ();
-    return FALSE;
-}
 
 /////////////////////////////////////////////
 ///////////CODIGO-MAIN//////////////////////
@@ -207,10 +253,12 @@ bool on_quit_clicked( GtkButton *button/*, gpointer   data*/ ) {
 		/* Start main loop */
 		gtk_main();
 
+	}
+
 		//////////////////////////////////////////////
 		///////manejo en terminal m_distortion////////
 		//////////////////////////////////////////////
-
+/*
 		char option;
 		unsigned char l = 0;
 		unsigned char r = 0;
@@ -283,6 +331,4 @@ bool on_quit_clicked( GtkButton *button/*, gpointer   data*/ ) {
 					break;		
 			}
 		}
-	}
-	
-
+	*/
