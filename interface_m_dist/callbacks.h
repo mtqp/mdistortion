bcb0e2830ddfdb274aebcbbd95fade06e1babd86
marian.sabianAA <marian.sabianAA@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-04-04 19:27:31 +0000

    sdf

---
+++ interface_m_dist/callbacks.h
@@ -0,0 +1,19 @@
+#ifndef __CALLBACKS_C__
+#define __CALLBACKS_C__
+
+#include <gtk/gtk.h>
+
+#ifndef bool
+    #define bool unsigned char
+    #define true 1
+    #define false 0
+#endif
+
+G_MODULE_EXPORT bool on_quit_clicked( GtkButton *button, gpointer   data );
+G_MODULE_EXPORT bool on_m_distortion_destroy (GtkObject *object, gpointer user_data);
+G_MODULE_EXPORT void on_rock_mode_clicked (gpointer distors, GtkRadioButton *button);
+G_MODULE_EXPORT void on_random_mode_clicked ( gpointer distors,GtkRadioButton *button);
+G_MODULE_EXPORT void on_by_pass_mode_clicked ( gpointer distors, GtkRadioButton *button);
+G_MODULE_EXPORT void on_mute_mode_clicked ( gpointer distors,GtkRadioButton *button);
+
+#endif
