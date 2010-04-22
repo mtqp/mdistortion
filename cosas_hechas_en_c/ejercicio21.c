1f262e0680a40db54f57357f16361b1e382f17d3
marian.sabianAA <marian.sabianAA@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-04-22 21:37:24 +0000

    ajap

--- cosas_hechas_en_c/ejercicio21.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-extern long long int suma (long long int a, long long int b);
-
-main() {
-	long long int res = 
-suma(0x1000000000000001LL,0x0000000000000001LL);
-	printf("%lld\n",res);
-
-}
