c799a6199aa003ea9ffe1e278c3c529b87329cb9
marian.sabianAA <marian.sabianAA@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-04-23 01:56:55 +0000

    a vergui

--- trunk/interface_m_dist/globals.h
+++ trunk/interface_m_dist/globals.h
@@ -1,6 +1,7 @@
 #include <jack/jack.h>
 #include "m_distortion.h"
 
+
 jack_port_t *input_port;
 jack_port_t *output_left;
 jack_port_t *output_right;
@@ -8,3 +9,8 @@ jack_port_t *output_right;
 m_distortion *m_dist; 
 
 int global_eq_sensitive; 
+float dt;
+float RC;
+float alpha;
+
+
