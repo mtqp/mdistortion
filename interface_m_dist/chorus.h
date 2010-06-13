4a762f6f04c5071da9b855d75cfa2ea53e4b3efd
marian.sabianaa <marian.sabianaa@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-06-13 21:46:33 +0000

    incredible

--- interface_m_dist/chorus.h
+++
@@ -1,5 +0,0 @@
-	int delay_size;
-	float* delay_buf;
-	float* delay_buf_2_orden;
-	float* delay_buf_3_orden;
-	float* delay_buf_4_orden;
---
+++ interface_m_dist/m_chorus.c
@@ -0,0 +1,34 @@
+
+#include "chorus.h"
+
+m_chorus* chorus_new(int size){
+	m_chorus* c = (m_chorus*) malloc(sizeof(m_chorus));
+	if(c == NULL){
+		printf("Couldn't malloc Chorus Structure\n");
+		return NULL;
+	}
+	else {
+		c->chr_size = size;
+		c->chr_buf1 = (float*) malloc(c->chr_size*sizeof(float));
+		c->chr_buf2 = (float*) malloc(c->chr_size*sizeof(float));
+		c->chr_buf3 = (float*) malloc(c->chr_size*sizeof(float));
+		c->chr_buf4 = (float*) malloc(c->chr_size*sizeof(float));
+		
+		if((c->chr_buf1 == NULL) || (c->chr_buf2 == NULL) || (c->chr_buf3 == NULL) || (c->chr_buf4 == NULL)) {
+			printf("Couldn't malloc Buffer Chorus Structure\n");
+			return NULL;
+		} else {
+			printf("	Chorus Effect Set\n");
+			int i;
+			for(i=0;i<c->chr_size;i++){
+				c->chr_buf1[i] = 0.0;
+				c->chr_buf2[i] = 0.0;
+				c->chr_buf3[i] = 0.0;
+				c->chr_buf4[i] = 0.0;
+			}
+			return d;
+		}
+						
+	}
+}
+
