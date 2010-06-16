022fafa3e932ef17d0d2e30932b7ebbedef4bde8
marian.sabianAA <marian.sabianAA@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-06-16 01:12:55 +0000

    wo tired

--- interface_m_dist/main.c
+++ interface_m_dist/main.c
@@ -122,6 +122,29 @@
 
 		/* Get main window pointer from UI */
 		window = GTK_WIDGET( gtk_builder_get_object( builder, "m_distortion" ) );
+		
+		//*Set Up m_user_interface Structure*//
+		m_ui = (m_user_interface*) malloc(sizeof(m_user_interface));
+		if(m_ui == NULL)	printf("Couldn't malloc UI Structure\n");
+		else {
+			m_ui->m_distortion 	= (GtkWindow*) 		window;
+			m_ui->box_eq 	   	= (GtkContainer*) 	gtk_builder_get_object(builder, "box_eq");
+			m_ui->eq_onoff 		= (GtkToggleButton*)gtk_builder_get_object(builder,"eq_onoff");
+			m_ui->extra_effects = (GtkContainer*) 	gtk_builder_get_object(builder,"extra_effects");
+			m_ui->delay 		= (GtkToggleButton*)gtk_builder_get_object(builder,"delay");
+			m_ui->hall 			= (GtkToggleButton*)gtk_builder_get_object(builder,"hall");
+			m_ui->eq_bars 		= (GtkContainer*) 	gtk_builder_get_object(builder,"eq_bars");
+			m_ui->distors 		= (GtkContainer*) 	gtk_builder_get_object(builder,"distors");
+			m_ui->hell_sqrt 	= (GtkRadioButton*) gtk_builder_get_object(builder,"hell_sqrt");
+			m_ui->volume 		= (GtkScaleButton*) gtk_builder_get_object(builder,"volume");
+			m_ui->m_delay 		= (GtkWindow*) 		gtk_builder_get_object(builder,"m_delay");
+			m_ui->dl_onoff 		= (GtkToggleButton*)gtk_builder_get_object(builder,"dl_onoff");
+			m_ui->dl_sets 		= (GtkContainer*) 	gtk_builder_get_object(builder,"dl_sets");
+			m_ui->m_hall 		= (GtkWindow*) 		gtk_builder_get_object(builder,"m_hall");
+			m_ui->hl_onoff 		= (GtkToggleButton*)gtk_builder_get_object(builder,"hl_onoff");
+			m_ui->chintensity 	= (GtkContainer*) 	gtk_builder_get_object(builder,"chintensity");
+		}
+		
 		/* Connect signals */
 		gtk_builder_connect_signals( builder, NULL );
 		/* Destroy builder, since we don't need it anymore */
