5e825c848c3726cef011446a155068ac8e6887d9
marian.sabianaa <marian.sabianaa@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-06-20 02:05:25 +0000

    aparentemente segfaultea xq no le alcanza el tiempo para hacer todo, q heavy!

--- interface_m_dist/m_delay.c
+++ interface_m_dist/m_delay.c
@@ -1,7 +1,7 @@
 
 #include "m_delay.h"
 
-m_delay* delay_new (int size){
+m_delay* delay_new (int size, int total){
 	m_delay* d = (m_delay*) malloc(sizeof(m_delay));
 	if(d == NULL){
 		printf("Couldn't malloc Delay Structure\n");
@@ -9,25 +9,43 @@ m_delay* delay_new (int size){
 	}
 	else {
 		d->dl_size = size;
-		d->dl_sub_i= 0;
+		d->dl_total_bufs = total;
 		d->dl_cant_bufs_active = 4;
+		d->dl_sub_i= 0;
 		d->dl_speed = size/2;	//puedo crecer 32 veces para arriva y 32 para abajo creo...
-		d->dl_buf1 = (float*) malloc(d->dl_size*sizeof(float));
-		d->dl_buf2 = (float*) malloc(d->dl_size*sizeof(float));
-		d->dl_buf3 = (float*) malloc(d->dl_size*sizeof(float));
-		
-		if((d->dl_buf1 == NULL) || (d->dl_buf2 == NULL) || (d->dl_buf2 == NULL)) {
+//		printf("LLEGAMOS A UNO\n");	
+
+		d->dl_bufs = (float**) malloc(d->dl_total_bufs*sizeof(float*));
+		if(d->dl_bufs == NULL){
 			printf("Couldn't malloc Buffer Delay Structure\n");
 			return NULL;
 		} else {
-			printf("	Delay Effect Set\n");
+//		printf("LLEGAMOS A dos\n");	
 			int i;
-			for(i=0;i<d->dl_size;i++){
-				d->dl_buf1[i] = 0.0;
-				d->dl_buf2[i] = 0.0;
-				d->dl_buf3[i] = 0.0;
+			for(i=0;i<d->dl_total_bufs;i++){
+				d->dl_bufs[i] = (float*) malloc(d->dl_size*sizeof(float));
 			}
-			return d;
+//		printf("LLEGAMOS A tres\n");	
+			int alguno_null = 0;	//ESTO ES FALSE!
+			for(i=0;i<d->dl_total_bufs;i++){
+				alguno_null |= (d->dl_bufs[i] == NULL);
+			}
+//		printf("LLEGAMOS A 4\n");	
+			if(alguno_null) {
+				printf("Couldn't malloc Buffer Delay Structure\n");
+				return NULL;
+			} else {
+				for(i=0;i<d->dl_total_bufs;i++){
+					printf("i==%d\n",i);
+					int j;
+					for(j=0;j<d->dl_size;j++){ 
+//					printf("j=%d\n",j);
+						d->dl_bufs[i][j] = 0.0;
+					}
+				}
+				printf("	Delay Effect Set\n");
+				return d;
+			}
 		}
 	}
 }
--- interface_m_dist/m_delay.h
+++ interface_m_dist/m_delay.h
@@ -8,11 +8,10 @@ typedef struct _m_delay {
 	int    dl_size;
 	int    dl_sub_i;
 	int	   dl_cant_bufs_active;	//1,2,3 o 4
+	int	   dl_total_bufs;
 	int    dl_speed;			//algo cercano a [65536 ... 262144] multiplo de 4096
-	float* dl_buf1;
-	float* dl_buf2;
-	float* dl_buf3;
+	float**dl_bufs;
 } m_delay;
 
-m_delay* delay_new (int size);
+m_delay* delay_new (int size, int total);
 #endif
