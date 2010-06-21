759718f75c60f52c5c08a37742fc75e85829d43c
marian.sabianaa <marian.sabianaa@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-06-21 21:14:15 +0000

    reorganizing

--- interface_m_dist/just_interface/m_window.h
+++
@@ -1,9 +0,0 @@
-#ifndef __M_WINDOW_H__
-#define __M_WINDOW_H__
-
-#include <gtk/gtk.h>
-#include <stdlib.h>
-
-GtkWindow* open_sub_window(char* window);
-
-#endif
--- interface_m_dist/m_distortion.sh
+++
@@ -1,3 +0,0 @@
-make
-jackd -dalsa $
-./m_distortion rock
