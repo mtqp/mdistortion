9bd9d988819c7e5015e2b95202d28eba3e7efcbd
marian.sabianAA <marian.sabianAA@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2010-06-27 16:15:02 +0000

    falta m_macros.asm

---
+++ debug-version/debug-ASM/m_hall.h
@@ -0,0 +1,16 @@
+#ifndef __HALL_H__
+#define __HALL_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _m_hall {
+	int    hll_size_bufs;
+	float  hll_coef;
+	float  hll_buf_quantity;
+	float**hll_bufs;
+} m_hall;				//Estructura HALL
+
+m_hall* hall_new(int size, int quantity);
+
+#endif
