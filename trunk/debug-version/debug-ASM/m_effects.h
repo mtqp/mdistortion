1bd1217112ab284ef45efb5ff89980cd9780fcb9
marian.sabianAA <marian.sabianAA@aceb03ae-2b9f-11df-a6fe-af80f4a49816>
2011-01-29 00:13:51 +0000

    con los function * en by_pass funcionando!

--- trunk/debug-version/debug-ASM/m_effects.h
+++ trunk/debug-version/debug-ASM/m_effects.h
@@ -9,13 +9,15 @@
 #include "m_delay.h"
 #include "m_hall.h"
 
-typedef float (EFFECT) (struct _m_distortion *md, float sample, int i);
+//version para C --> typedef float (EFFECT) (struct _m_distortion *md, float sample, int i);
+/*version ASM*/
+typedef float (EFFECT) (struct _m_distortion *md, int i);
 
-EFFECT equalizer_func;
-EFFECT delay_func;
-EFFECT hall_func;
+extern EFFECT equalizer_func;
+extern EFFECT delay_func;
+extern EFFECT hall_func;
+extern EFFECT dummy_func;
 //EFFECT volume_func;
-EFFECT dummy_func;
 
 EFFECT * f_effect[5];
 EFFECT * equalizer_effect;
@@ -26,7 +28,7 @@ EFFECT * volume_effect; //todavia volume no se usa para nada
 void init_m_effects(struct _m_distortion* md);
 void eq_new(struct _m_distortion* md);
 
-extern void hall_asm(struct _m_distortion* md, int buf_size);
-extern void dummy_asm(struct _m_distortion* md, int buf_size);
+/*extern void hall_asm(struct _m_distortion* md, int buf_size);
+extern void dummy_asm(struct _m_distortion* md, int buf_size);**/
 
 #endif
